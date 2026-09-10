762856e412aab6a78cb49277262afbdf7d1a654f
zala-Prog <zalajyotindrasinh17@gmail.com>
2026-09-10 11:35:11 +0530

    Add P1.11 Date Function Playground

---
+++ Unit 1/P1.11_Date_Function_Playground.sql
@@ -0,0 +1,11 @@
+SET SERVEROUTPUT ON;
+DECLARE
+  v_n NUMBER := MOD(36, 12) + 1;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Today: ' || TO_CHAR(SYSDATE, 'Day, DD "th" Month YYYY'));
+  DBMS_OUTPUT.PUT_LINE('After ' || v_n || ' months: ' || TO_CHAR(ADD_MONTHS(SYSDATE, v_n), 'DD-MON-YYYY'));
+  DBMS_OUTPUT.PUT_LINE('Last day of current month: ' || TO_CHAR(LAST_DAY(SYSDATE), 'DD-MON-YYYY'));
+  DBMS_OUTPUT.PUT_LINE('Next Monday: ' || TO_CHAR(NEXT_DAY(SYSDATE, 'MONDAY'), 'DD-MON-YYYY'));
+  DBMS_OUTPUT.PUT_LINE('Days left in month: ' || TRUNC(LAST_DAY(SYSDATE) - SYSDATE));
+END;
+/
